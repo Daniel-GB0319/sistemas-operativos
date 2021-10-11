ed7c855bac0043491548eb2bbfb9215577b1c846
Daniel Gonzalez B <daniel.gb0319@gmail.com>
2021-10-10 20:27:50 -0500

    Primera version funcional. Arreglar detalles

--- Practicas/Practica2/Practica2_cliente_hijos.c
+++ Practicas/Practica2/Practica2_cliente_hijos.c
@@ -13,92 +13,51 @@ int personas, Confirmacion;
 
 void piper(int fd) //Recepción de asientos
 {
-    //int buf[10000];
     int n;
     fd = open("/tmp/mi_fifo", O_RDONLY);
-
-    for (int i = 0; i < 5; i++)
-    {
+    for (int i = 0; i < 5; i++){
         n = read(fd, &buf[i], sizeof(int));
-        printf("Asiento: %d\n", buf[i]);
+        printf("Lugar #%d: %d\n",i, buf[i]);
     };
-
-    //printf("Bites : %d\n", n);
-
     close(fd);
 }
 
 void pipew(int fd, int buf[]) //Tubería de asignación de asientos
 {
     mkfifo("/tmp/mi_fifo", 0666);
-
     fd = open("/tmp/mi_fifo", O_WRONLY);
-
-    for (int i = 0; i < personas; i++)
-    {
+    for (int i = 0; i < personas; i++){
         write(fd, &buf[i], sizeof(int));
     }
-
     close(fd);
 }
 
-void pipewv(int fd, int buf[]) //vuelos
-{
-    mkfifo("/tmp/mi_fifo", 0666);
-
-    fd = open("/tmp/mi_fifo", O_WRONLY);
-
-    write(fd, &buf[0], sizeof(int));
-
-    close(fd);
-}
-
-void piperc(int fd) //Pipe de confirmación
-{
-
+void piperc(int fd){ //Pipe de Confirmacion
     fd = open("/tmp/mi_fifo", O_RDONLY);
-
     read(fd, bufc, sizeof(bufc));
-
     printf("\nEstatus: %s\n", bufc);
-
     close(fd);
 }
 
-void pipemenu(int Confirmacion)
-{
+void pipemenu(int Confirmacion){
     mkfifo("/tmp/mi_fifo", 0666);
-
     fd = open("/tmp/mi_fifo", O_WRONLY);
-
     write(fd, &Confirmacion, sizeof(int));
-
     close(fd);
 }
 
-void menu()
-{
-    //Impresión en consola
-    printf("\n\t***Aeropuerto Internacional de la Ciudad de Mexico***\n");
-    printf("\n\t***Reservas de vuelos***\n");
-    printf("\nOpciones:");
-    printf("\n1: Vuelo a Cancun, viaje redondo");
-    printf("\n2: Vuelo a Yucatan, viaje redondo");
-    printf("\n3: Vuelo a Sinaloa, viaje redondo");
-    printf("\n4: Salir");
-    printf("\nSeleccione su reservacion al introducir el numero:\n");
-    printf("\n Ejemplo: 1\n");
-    printf("Seleccion:\n");
-    scanf("%d", &buf[0]);
-    if (buf[0] == 4)
-    {
-        exit(0);
-    }
-    //Tubería de selección de vuelo
-    pipewv(fd, buf);
+void menu(){
+    printf("\n\t*** Practica2 -- Procesos ***\n");
+    printf("\n\t*** Compra de Boletos ***\n");
+    // Cliente avisa a Servidor que se ha conectado
+    mkfifo("/tmp/mi_fifo", 0666);
+    fd = open("/tmp/mi_fifo", O_WRONLY);
+    write(fd, &buf[0], 1);
+    close(fd);
 
     //Tubería de recepción de asientos
-    printf("\nLos asientos disponibles son:\n");
+    printf("\nLos lugares para el vuelo son:\n");
+    printf("1 = Disponible para Compra / 0 = Ocupado\n\n");
     piper(fd);
     printf("\n");
 
@@ -109,14 +68,11 @@ void menu()
     //pipe de menu de confirmación
     pipemenu(Confirmacion);
 
-    if (Confirmacion == 1)
-    {
-
+    if (Confirmacion == 1){
         //Tubería de reserva de asientos
         printf("Indique el numero de personas para el vuelo:\n");
         scanf("%d", &personas);
-        for (int i = 0; i < personas; i++)
-        {
+        for (int i = 0; i < personas; i++){
             printf("Asiento del Pasajero %d:\n", i);
             scanf("%d", &buf[i]);
         }
@@ -124,9 +80,7 @@ void menu()
 
         //Tubería de confirmación de estatus de la reserva
         piperc(fd);
-    }
-    else
-    {
+    }else{
         menu();
     }
 }
--- Practicas/Practica2/Programa2_Servidor_hijos.c
+++ Practicas/Practica2/Programa2_Servidor_hijos.c
@@ -5,33 +5,25 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
-
-//procesos hijos
-#include <unistd.h>
 #include <wait.h>
 #include <sys/types.h>
 
 //Variables globales
 int fd = -1;
 pid_t pid;
-int asientos[] = {1, 2, 3, 4, 5};
-int asientos2[] = {1, 2, 3, 4, 5};
-int asientos3[] = {1, 2, 3, 4, 5};
+int asientos[5];
 char bufres[] = "1,2,3,4,5";
 char confirm[] = "Vuelo y asientos reservados correctamente";
 char asientooc[] = "El asiento esta ocupado";
 char vuelolleno[] = "El vuelo esta lleno";
-int bufc, N_vuelo = 0;
+int bufc;
 
-int binarySearch(int arr[], int l, int r, int x)
-{
-    while (l <= r)
-    {
+int binarySearch(int arr[], int l, int r, int x){
+    while (l <= r){
         int m = l + (r - l) / 2;
         int dato = 0;
         // Check if x is present at mid
-        if (arr[m] == x)
-        { //Se verifica si el valor buscado esta en la m-esima posicion y regresa el dato
+        if (arr[m] == x){ //Se verifica si el valor buscado esta en la m-esima posicion y regresa el dato
             dato = x;
             arr[m] = 0;
             return dato;
@@ -49,37 +41,6 @@ int binarySearch(int arr[], int l, int r, int x)
     return -1;
 }
 
-void piper(int fd) //lectura de vuelo
-{
-    int buf[10000];
-    int n;
-    fd = open("/tmp/mi_fifo", O_RDONLY);
-
-    n = read(fd, &buf[0], sizeof(int));
-
-    //printf("Bites : %d\n", n);
-    printf("Cliente: %d\n", buf[0]);
-    switch (buf[0])
-    {
-    case 1:
-        printf("\n1: Vuelo a Cancun, viaje redondo\n");
-        N_vuelo = 1;
-        break;
-    case 2:
-        printf("\n2: Vuelo a Yucatan, viaje redondo\n");
-        N_vuelo = 2;
-        break;
-    case 3:
-        printf("\n3: Vuelo a Sinaloa, viaje redondo\n");
-        N_vuelo = 3;
-        break;
-    default:
-        printf("\nError en vuelo\n");
-        break;
-    }
-
-    close(fd);
-}
 
 void pipera(int fd, int asientos[]) //Lectura de la reserva de asientos el 5 es el tamaño de los asientos
 {
@@ -90,128 +51,84 @@ void pipera(int fd, int asientos[]) //Lectura de la reserva de asientos el 5 es 
     for (int i = 0; i < 5; i++)
     {
         n = read(fd, &buf[i], sizeof(int));
-        //printf("Asiento del Cliente: %d\n", buf[i]);
         reserva[i] = binarySearch(asientos, 0, 5, buf[i]);
-        if (reserva[i] != -1)
-        {
+        if (reserva[i] != -1){
             printf("Asiento del Cliente: %d\n", reserva[i]);
         }
     }
-    //printf("Bites : %d\n", n);
-    for (int i = 0; i < 5; i++)
-    {
+    for (int i = 0; i < 5; i++){
         printf("\nAsiento disponible %d\n", asientos[i]);
     }
-
     close(fd);
 }
 
 void pipewc(int fd) //Pipe de confirmacion
 {
     mkfifo("/tmp/mi_fifo", 0666);
-
     fd = open("/tmp/mi_fifo", O_WRONLY);
-
     write(fd, confirm, sizeof(confirm));
-
-    //printf("%s\n", confirm);
-
     close(fd);
 }
 
-void pipew(int fd, int buf[]) //Pipe de envio de asientos
-{
-    mkfifo("/tmp/mi_fifo", 0666);
-
-    fd = open("/tmp/mi_fifo", O_WRONLY);
-
-    for (int i = 0; i < 5; i++)
-    {
-        write(fd, &buf[i], sizeof(int));
-    }
-
-    close(fd);
-}
-
-void pipemenur(int fd)
-{
+void pipemenur(int fd){
     int n;
     fd = open("/tmp/mi_fifo", O_RDONLY);
-
     n = read(fd, &bufc, sizeof(int));
-
-    //printf("Bites : %d\n", n);
     printf("Confirmacion: %d\n", bufc);
 }
 
-int main(void)
-{
+int main(void){
+    int buf[10000];
+    int n, x = 0 ;
 
-    //Recepcion de pipes
-    while (fd == -1)
-    {
-        pid = fork();
-        if (pid == -1)
-        {
-            perror("\nError al crear el proceso\n");
-            exit(-1);
-        }
-        if (pid == 0)
-        {
-            printf("\nHola soy el proceso hijo\n");
-            printf("\nMi identificador es: %d\n", getpid());
-            printf("\nMi proceso padre es: %d\n", getppid());
+    // se inicializan lugares a disponibles
+    for(n=0;n<5;n++){
+        asientos[n]=1;
+    }
+    n=0;
+    //Bucle de servidor Infinito 
+    while (x != 1){
+        printf("SERVIDOR ACTIVO\n\nESPERANDO A UN CLIENTE...");
+        fd = open("/tmp/mi_fifo", O_RDONLY);
+        n = read(fd, &buf[0], sizeof(int));
+        
+        if(n==1){ // Cliente se ha conectado
+            pid = fork();
+            if (pid == -1){
+                perror("\nError al crear el proceso\n");
+                exit(-1);
+            }
+        if (pid == 0){
+            printf("\nSoy el Hijo = %d y mi Padre es = %d\n",getpid(),getppid());      
+            printf("!!! Cliente Conectado !!!");
+            
+            //Se envian los boletos disponibles
+            mkfifo("/tmp/mi_fifo", 0666);
+            fd = open("/tmp/mi_fifo", O_WRONLY);
+            for (int i = 0; i < 5; i++){
+                write(fd, &buf[i], sizeof(int));
+            }
+            close(fd);
 
-            piper(fd); //tuberia de lectura de vuelo
-            if (N_vuelo == 1)
-            {
-                pipew(fd, asientos); //tuberia de envio de asientos disponibles
-                printf("\n1N_vuelo\n");
-            }
-            if (N_vuelo == 2)
-            {
-                pipew(fd, asientos2); //tuberia de envio de asientos disponibles
-                printf("\n2N_vuelo\n");
-            }
-            if (N_vuelo == 3)
-            {
-                pipew(fd, asientos3); //tuberia de envio de asientos disponibles
-                printf("\n3N_vuelo\n");
-            }
-
+            printf("\nLista de Boletos Disponibles Enviados\n");
+            
             pipemenur(fd); //Tuberia de confirmacion de compra de vuelos
-            if (bufc == 1)
-            {
-                //pipera(fd); //tuberia de reserva de asientos
-                if (N_vuelo == 1)
-                {
-                    pipera(fd, asientos); //tuberia de envio de asientos disponibles
-                }
-                if (N_vuelo == 2)
-                {
-                    pipera(fd, asientos2); //tuberia de envio de asientos disponibles
-                }
-                if (N_vuelo == 3)
-                {
-                    pipera(fd, asientos3); //tuberia de envio de asientos disponibles
-                }
+            if (bufc == 1){
+                pipera(fd, asientos); //tuberia de envio de asientos disponibles
                 pipewc(fd); //Tuberia de confirmacion
             }
-            //sleep(10);
-            //exit(0);
-        }
-        else
-        {
+        }else{
             wait(NULL);
             printf("\n\t\t\tHola soy el proceso padre\n");
             printf("\n\t\t\tMi identificador es: %d\n", getpid());
             printf("\n\t\t\tMi proceso padre es: %d\n", getppid());
-            //piper(fd);
         }
         printf("\n\t\t\tHola soy el proceso padre fuera del if\n");
         printf("\n\t\t\tMi identificador es: %d\n", getpid());
         printf("\n\t\t\tMi proceso padre es: %d\n", getppid());
+
+        }
+        system("clear");
     }
-    printf("\nFin\n");
     return 0;
 }
