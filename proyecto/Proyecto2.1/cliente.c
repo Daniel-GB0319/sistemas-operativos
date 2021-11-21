96830eb2c8ed0b3f39b01ca699190466962f44be
Daniel Gonzalez B <daniel.gb0319@gmail.com>
2021-11-21 05:22:04 -0600

    Proyecto 2.1 agregado.

---
+++ proyecto/Proyecto2.1/cliente.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include "../include/systemv.h"
+
+int main(void)
+{
+    int contador;
+    int sem_servidor;
+    int sem_cliente;
+    char numero_acceso[3]; // Para convertir el número de acceso a cadena
+    char archivo_acceso[15]; // Para alamcenar el nombre del archivo para la llave de cada asiento
+    int *acceso = NULL;
+    int *claves_comunicacion_hilo;
+    int llave, archivo;
+
+    /* ------------------------- Memorias compartidas ------------------------- */
+
+    /* Conexión al arreglo de acceso */
+    acceso = (int *)shm(sizeof(int)*20, "shm_acceso", 'u');
+
+    /* Conexión a la memoria compartida para la asignación de claves de comunicación con el hilo */
+    claves_comunicacion_hilo = (int *)shm(sizeof(int)*2, "shm_parametros_cliente", 'v');
+
+    /* ------------------------------ Semáforos ------------------------------- */
+
+    /* Semáforo del servidor */
+    sem_servidor = sem(0, "sem_servidor", 'w');
+
+    /* Semáforo del cliente */
+    sem_cliente = sem(0, "sem_cliente", 'x');
+
+    /* Semáforos de los acceso */
+    for (contador = 0; contador < 20; contador++) {
+        /* Creación del nombre del archivo de la clave del asiento en el índice @contador */
+        sprintf(numero_acceso, "%d", contador+1);
+        strcpy(archivo_acceso, "sem_acceso_");
+        strcat(archivo_acceso, numero_acceso);
+        
+        acceso[contador] = sem(1, archivo_acceso, contador+100);
+    }
+
+    /* --------------------------- Configuraciones ---------------------------- */
+
+
+    /* ----------------- Obtención de claves de comunicación ------------------ */
+
+    up(sem_servidor);
+    printf("Obteniendo llaves...\n");
+    down(sem_cliente);
+
+    llave = claves_comunicacion_hilo[0];
+    archivo = claves_comunicacion_hilo[1];
+
+    up(sem_servidor);
+
+    printf("Llaves obtenidas...\n");
+    printf("shm: %c, sem: %d\n", llave, archivo);
+
+    return 0;
+}
--- proyecto/Proyecto2.1/src/carrito.c
+++ proyecto/Proyecto2.1/src/carrito.c
@@ -1,43 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Carrito_Compras {
-	char name[21]; // Nombre del Producto
-	int cant; // Cantidad del Producto
-	int precio; // Precio del producto		
-} carrito;
-
-typedef struct nodo {
-    carrito Inf;
-    struct nodo * sig;
-	struct nodo * ant;  	
-} NODO;
-
-typedef struct Cab {
-	NODO * I;
-	NODO * F;
-	int NE;
-} cabecera;
-
-
-typedef  cabecera * lista;
-typedef  NODO * linkList;
-typedef  enum men { NO_MEMORY , OK , INDEXOUTOFBOUND, EMPTY } mensaje;
-typedef  enum b { FALSE , TRUE } booleano;
-
-
-// %%% Prototipos %%% 
-carrito get ( int pos , lista l );  
-linkList ubicar ( int pos , lista l );                                        
-mensaje add ( int pos, carrito E , lista l );         
-mensaje borrar (  int Pos , lista l );                   
-booleano empty ( lista l  );
-mensaje set ( int Pos ,  carrito E , lista l);      
-mensaje  crearlista( lista * l ); 
-void  liberarlista( lista * l );  	   
-void vaciarlista( lista l ); 
-
-
 // %%% Funciones %%%
 carrito get ( int pos , lista l ) {
 	carrito copia;
